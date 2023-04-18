d6afbf8b180a97193c8ba87a3933b52a9ed913cb
Cesar Mejia <cesar.mejia@softwareone.com>
2023-04-18 17:37:09 -0500

    Primer commit ejercicio Unit Test

---
+++ ASPTest/Controllers/BeerController.cs
@@ -0,0 +1,28 @@
+﻿using ASPTest.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        private readonly IBeerService _beerService;
+
+        public BeerController(IBeerService beerService)
+        {
+            _beerService= beerService;
+        }
+        [HttpGet]
+        public IActionResult Get() =>Ok(_beerService.Get());
+        [HttpGet]
+        public IActionResult GetById(int Id)
+        {
+            var beer = _beerService.Get(Id);
+            if(beer==null)
+                return NotFound();
+            return Ok(beer);
+        }
+    }
+}
